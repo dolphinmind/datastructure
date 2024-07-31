b23d5c59b43cbecb8466b846b05ab0ecf385857c
dolphinmind <dolphinmind@foxmail.com>
2024-07-31 17:04:39 +0800

    feat:array-binary-search-02：测试

---
+++ array/src/test/java/com.github.dolphinmind.array.binarusearch/ApiTest.java
@@ -0,0 +1,38 @@
+package com.github.dolphinmind.array.binarusearch;
+
+import com.github.dolphinmind.array.binarysearch.BinarySearch;
+import org.junit.Test;
+
+/**
+ * @author dolphinmind
+ * @ClassName ApiTest
+ * @description
+ * @date 2024/7/31
+ */
+
+public class ApiTest {
+
+    /**
+     * 找房子编号，不要找房子里面的内容
+     */
+    @Test
+    public void test_binarySearchRightClose() {
+
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int result = binarySearch.binarySearchRightClose(nums, target);
+        System.out.println("左闭右闭区间选择：" + result);
+    }
+
+    @Test
+    public void test_binarySearchRightOpen() {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 2;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int result = binarySearch.binarySearchRightOpen(nums, target);
+        System.out.println("左闭右开区间选择：" + result);
+    }
+}
