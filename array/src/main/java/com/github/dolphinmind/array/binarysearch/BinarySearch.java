79258422e0ac75d417ce4ab75c66f21cf64f34d5
dolphinmind <dolphinmind@foxmail.com>
2024-08-01 09:59:56 +0800

    feat:array-binary-search-05-增加左边界索引和右边界索引方法

--- array/src/main/java/com/github/dolphinmind/array/binarysearch/BinarySearch.java
+++ array/src/main/java/com/github/dolphinmind/array/binarysearch/BinarySearch.java
@@ -21,7 +21,6 @@ public class BinarySearch {
     /**
      * 二分法搜索：左闭右闭区间
      * 分析：循环条件看索引，中间索引采用移位，逻辑判断条件优先选取目标，循环不成立有兜底 -1
-     *      left 处于有效数组的末尾，即不包含target的元素的最后一个位置之后，返回的left即有效数组的长度
      * @param nums
      * @param target
      * @return
@@ -70,6 +69,90 @@ public class BinarySearch {
 
     }
 
+    /**
+     * <p>
+     *     左右边界索引的二分查找，适用于查找左边界和右边界，但同样适应于数组中只存在一个target的情况
+     *     假设target = nums[mid]一开始就成立
+     *     尽管在第一次比较时，right或left可能看似"跳过了"目标值，
+     *     但由于target唯一性，后续的搜索范围更新操作不会导致left或right错过target
+     *     对于的就是左边界索引的left = mid + 1, 有边界索引的 right = mid - 1
+     *     left 或 right都会正确地指向target的索引位置，成为有效的左边界或有边界索引
+     * </p>
+     */
 
+    /**
+     * 前置条件：有序数组，元素可允许可重复
+     * Left Bound Index ,指的是目标值首次出现的位置，如果数组中有多个相同的元素(即重复值)，左边界索引是目标值在数组中的最早出现位置
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int leftBound(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + ((right - left) >> 2);
+            if (target == nums[mid]) {
+                right = mid - 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        // 增加边界检查
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+
+    /**
+     * 前置条件：有序数组，元素可允许可重复
+     * Right Bound Index ,指的是目标值最后出现的位置，如果数组中有多个相同的元素(即重复值)，右边界索引是目标值在数组中的最晚出现位置
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int rightBound(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 2);
+
+            if (target == nums[mid]) {
+                left = mid + 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            }
+        }
+
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+
+        return right;
+    }
 
 }
--- array/src/test/java/com.github.dolphinmind.array.binarusearch/BinarySearchTest.java
+++ array/src/test/java/com.github.dolphinmind.array.binarusearch/BinarySearchTest.java
@@ -36,4 +36,26 @@ public class BinarySearchTest {
         int result = binarySearch.binarySearchRightOpen(nums, target);
         System.out.println("左闭右开区间选择：" + result);
     }
+
+
+    @Test
+    public void test_leftBound() {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int result = binarySearch.leftBound(nums, target);
+        System.out.println("左边界索引：" + result);
+    }
+
+    @Test
+    public void test_rightBound() {
+        int[] nums = {-1,0,3,5,9,12};
+//        int[] nums = {};
+        int target = 2;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int result = binarySearch.rightBound(nums, target);
+        System.out.println("右边界索引：" + result);
+    }
 }
