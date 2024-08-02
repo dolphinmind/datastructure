85382838aaac2801894c03a1773311922614527c
dolphinmind <dolphinmind@foxmail.com>
2024-08-02 20:29:20 +0800

    fix:range-sum-query-immutable

--- array/src/main/java/com/github/dolphinmind/array/RangeSumQueryImmutable.java
+++ array/src/main/java/com/github/dolphinmind/array/RangeSumQueryImmutable.java
@@ -11,11 +11,19 @@ public class RangeSumQueryImmutable {
 
     // 前缀和数组 缓存概念
     private int[] preSum;
+    private int[] recoverArray;
+    private int length;
 
     public RangeSumQueryImmutable(int[] nums) {
 
-        // 初始化前缀和数组 preSum[0]=0
-        preSum = new int[nums.length + 1];
+
+        length = nums.length;
+
+        // 初始化前缀和数组 preSum[0]=0, 允许空数组的存在
+        preSum = new int[length + 1];
+
+        // 初始化恢复数组 recoverArray
+        recoverArray = new int[length];
 
         for (int i = 1; i < preSum.length; i++) {
             preSum[i] = preSum[i-1] + nums[i-1];
@@ -23,7 +31,56 @@ public class RangeSumQueryImmutable {
 
     }
 
-    public int sunRange(int left, int right) {
-        return preSum[right+1] - preSum[left];
+    public int sumRange(int left, int right) {
+
+        if (left < 0 || right >= preSum.length - 1) {
+            System.out.print("索引超出数组范围: ");
+            return -1;
+        }
+
+        if (left == right) {
+            System.out.print("索引相同: ");
+            return 0;
+        }
+
+        if (left > right) {
+            System.out.print("索引顺序错误: ");
+            return preSum[left+1] - preSum[right];
+        }
+
+        System.out.print("索引顺序正确: ");
+        return preSum[right + 1] - preSum[left];
+
+    }
+
+    public void recoverArray() {
+        for (int i = length-1 ; i >= 0; i--) {
+            recoverArray[i] = preSum[i+1]-preSum[i];
+        }
+    }
+
+    public void printArray(int[] nums) {
+        System.out.println();
+
+        System.out.print("[");
+        for (int item : nums) {
+            System.out.print(item + " ");
+        }
+        System.out.print("]");
+    }
+
+    public void printOriginArray(int[] nums) {
+        System.out.print("\n原始数组：");
+        printArray(nums);
+    }
+    public void printPreSum() {
+        System.out.print("\n前缀和数组：");
+        printArray(preSum);
+    }
+
+    public void printRecoverArray() {
+        this.recoverArray();
+        System.out.print("\n复原数组: ");
+        printArray(recoverArray);
     }
 }
--- array/src/test/java/com/github/dolphinmind/array/RangeSumQueryImmutableTest.java
+++ array/src/test/java/com/github/dolphinmind/array/RangeSumQueryImmutableTest.java
@@ -13,10 +13,22 @@ public class RangeSumQueryImmutableTest {
 
     @Test
     public void test_sunRange() {
-        int[] nums = {-2, 0, 3, -5, 2, -1};
+//        int[] nums = {-2, 0, 3, -5, 2, -1};
+//        int[] nums   =  {   1, 2, 3, 4,  5,  6,  7,  8};
+        int[] nums   = {};
+
         RangeSumQueryImmutable rangeSumQueryImmutable = new RangeSumQueryImmutable(nums);
-        System.out.println(rangeSumQueryImmutable.sunRange(0, 2));
-        System.out.println(rangeSumQueryImmutable.sunRange(2, 5));
-        System.out.println(rangeSumQueryImmutable.sunRange(0, 5));
+
+        rangeSumQueryImmutable.printOriginArray(nums);
+        rangeSumQueryImmutable.printPreSum();
+        rangeSumQueryImmutable.printRecoverArray();
+
+        System.out.println("\n");
+        System.out.println(rangeSumQueryImmutable.sumRange(2, 2));
+        System.out.println(rangeSumQueryImmutable.sumRange(0, 1));
+        System.out.println(rangeSumQueryImmutable.sumRange(1, 0));
+        System.out.println(rangeSumQueryImmutable.sumRange(1, 9));
+        System.out.println(rangeSumQueryImmutable.sumRange(0, 7));
+
     }
 }
